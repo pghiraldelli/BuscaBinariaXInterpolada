f6b1467ad98396983d90e0779bfa77e0edb4cf1f
pghiraldelli <patriciaghiraldelli@gmail.com>
2016-09-26 21:20:01 -0300

    Adicionando funcoes de exemplo de melhor numero de iteracoes para cada algoritmo

--- binXinterp.py
+++ binXinterp.py
@@ -1,5 +1,6 @@
-import numpy
+import random
 
+'''Busca um elemento em uma lista, partindo sempre do meio do array e comparando as duas metades.'''
 def buscaBinaria(lista, valor):
   limiteSuperior = len(lista)-1
   limiteInferior = 0
@@ -10,12 +11,14 @@ def buscaBinaria(lista, valor):
     meio = (limiteInferior + limiteSuperior)/2
 
     if (lista[meio] == valor): 
-      return [True, totalIteracoes]
+      return ["Achou", totalIteracoes]
     elif (lista[meio] > valor): limiteSuperior = meio - 1
     else: limiteInferior = meio + 1
 
-  return [False, totalIteracoes]
+  return ["Nao achou", totalIteracoes]
 
+'''Busca um elemento numa lista, levando em consideracao uma distribuicao uniforme, calculando a posicao
+onde o elemento se encontra a partir de seu valor e incrementando de 1 caso nao o encontre na primeira vez. '''
 def buscaInterpolada(lista, valor):
   limiteSuperior = len(lista)-1
   limiteInferior = 0
@@ -25,27 +28,58 @@ def buscaInterpolada(lista, valor):
     totalIteracoes += 1
     fracao = (valor - lista[limiteInferior]) // (lista[limiteSuperior] - lista[limiteInferior])
     meio = limiteInferior + (limiteSuperior - limiteInferior) * fracao
-
     if (lista[meio] == valor): 
-      return [True, totalIteracoes]
+      return ["Achou", totalIteracoes]
     elif (lista[meio] > valor): limiteSuperior = meio - 1
     else: limiteInferior = meio + 1
 
-  return [False, totalIteracoes]
+  return ["Nao achou", totalIteracoes]
 
+'''Gera uma lista ordenada e sem repeticao de numeros aleatorios de zero a tamanhoDaLista*10.'''
 def geraListaAleatoriaOrdenada(tamanho):
-  lista = numpy.random.randint(0, tamanho, tamanho)
+  lista = random.sample(range(tamanho*10), tamanho)
   lista.sort()
   return lista
 
+'''Gera uma lista ordenada e sem repeticao que nao segue uma distribuicao uniforme. Ela e preenchida com zeros ate a metade e depois preenchida com valores maiores que zero e menores que 50.'''
+def geraListaBinariaMelhor():
+  tamanho = 10000
+  lista = []
+  num = 1
+  for i in range(0,tamanho):
+	if i < tamanho/2: lista.append(0)
+	else: 
+	  lista.append(num)
+	  num += 1
+  return lista
+
+'''Gera uma lista ordenada e sem repeticao, seguindo uma distribuicao uniforme de tamanho 10000.'''
+def geraListaInterpoladaMelhor():
+  tamanho = 100
+  lista = []
+  for i in range (0,tamanho):
+	lista.append(i*2)
+  return lista
+
 def main():
-  lista = geraListaAleatoriaOrdenada(100)
+  geradorLista = int(raw_input("Digite (1) para gerar uma lista aleatoria, (2) para um exemplo em que a busca binaria seja melhor e (3) para um exemplo cuja a interpolada seja melhor: "))
+
+  lista = []
+  if geradorLista == 1 : 
+	tamanho = int(raw_input("Digite o tamanho pretendido para a lista: "))
+	lista = geraListaAleatoriaOrdenada(tamanho)
+  elif geradorLista == 2 : lista = geraListaBinariaMelhor()
+  elif geradorLista == 3 : lista = geraListaInterpoladaMelhor()
+
   print lista
+
+  valor = int(raw_input("Digite um valor a ser procurado na lista: "))
+
   print "Binaria"
-  solBin = buscaBinaria(lista, 9)
+  solBin = buscaBinaria(lista, valor)
   print solBin
   print "Interpolada"
-  solInt = buscaInterpolada(lista, 9)
+  solInt = buscaInterpolada(lista, valor)
   print solInt
 
 
